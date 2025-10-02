1919565a61aceddf511be467dce5e30a3d203870
Bhutite <tenzin.bhutia@tecnotree.com>
2025-10-02 21:15:40 +0530

    middleware changes

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,26 +1,30 @@
 import express from "express";
-import { getAllMessages, getUnseenMessagesCount, markMessagesAsSeen, sendMessage, sendMessageToUser,getRecentChats,getSharedMedia } from "../controllers/messageController.js";
+import { getAllMessages, getUnseenMessagesCount, markMessagesAsSeen, sendMessageToUser, getRecentChats, getSharedMedia } from "../controllers/messageController.js";
 import { protectRoute } from "../middleware/auth.js";
 
 const messageRouter = express.Router();
 
-// CORS middleware for this router
-// messageRouter.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-//     if (req.method === 'OPTIONS') {
-//         return res.status(200).end();
-//     }
-//     next();
-// });
+// --- CORS middleware ---
+messageRouter.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://chat-app-frontend-pearl.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-messageRouter.get("/unseen-count",protectRoute,getUnseenMessagesCount);
-messageRouter.get("/all-messages/:id",protectRoute,getAllMessages);
-messageRouter.put("/mark-seen/:id",protectRoute,markMessagesAsSeen);
-messageRouter.post("/send-message/:id",protectRoute,sendMessageToUser);
-messageRouter.get("/recent-chats/:id",protectRoute,getRecentChats);
-messageRouter.get("/get-shared-media/:id",protectRoute,getSharedMedia)
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+// --- Routes ---
+messageRouter.get("/unseen-count", protectRoute, getUnseenMessagesCount);
+messageRouter.get("/all-messages/:id", protectRoute, getAllMessages);
+messageRouter.put("/mark-seen/:id", protectRoute, markMessagesAsSeen);
+messageRouter.post("/send-message/:id", protectRoute, sendMessageToUser);
+messageRouter.get("/recent-chats/:id", protectRoute, getRecentChats);
+messageRouter.get("/get-shared-media/:id", protectRoute, getSharedMedia);
 
 export default messageRouter;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,23 +4,27 @@ import { protectRoute } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
-// CORS middleware for this router
-// userRouter.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-//     if (req.method === 'OPTIONS') {
-//         return res.status(200).end();
-//     }
-//     next();
-// });
+// --- CORS middleware ---
+userRouter.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://chat-app-frontend-pearl.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+// --- Routes ---
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
-userRouter.put("/update-profile",protectRoute,updateProfile);
-userRouter.get("/check-auth",protectRoute,checkAuth);
-userRouter.get("/all-users",protectRoute,allUsers);
-userRouter.get("/searchUser/:search", protectRoute,searchUser)
+userRouter.put("/update-profile", protectRoute, updateProfile);
+userRouter.get("/check-auth", protectRoute, checkAuth);
+userRouter.get("/all-users", protectRoute, allUsers);
+userRouter.get("/searchUser/:search", protectRoute, searchUser);
 
 export default userRouter;
