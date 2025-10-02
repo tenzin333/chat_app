86e89540c0d9981bc42b69a8f3380e70383349fa
Bhutite <tenzin.bhutia@tecnotree.com>
2025-10-02 20:38:07 +0530

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
 import react, { useContext } from 'react';
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from './layout/LoginPage';
 import ProfilePage from './layout/ProfilePage';
-import {Toaster} from 'react-hot-toast';  
+import { Toaster } from 'react-hot-toast';
 import { AuthContext } from './context/AuthContext';
 import HomePage from './layout/HomePage';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const App = () => {
-  const {authUser,onlineUsers,login,logout,updateProfile,axios} = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { authUser } = useContext(AuthContext);
   return (
     <div className="bg-[url('/bg.jpg')] bg-contain text-white">
-      <Toaster/>
+      <Toaster />
       <Routes>
-        <Route  path="/" element={authUser ? <HomePage/> : navigate("/login")} />
-        <Route  path="/login" element={!authUser ? <LoginPage/>: navigate("/")} />
-        <Route  path="/profile" element={authUser ? <ProfilePage/> : navigate("/login")} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" replace />}
+        />
       </Routes>
     </div>
   )
