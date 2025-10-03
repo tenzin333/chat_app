d5647174e896fbe7aa3288b2e3dc8488bf005977
Bhutite <tenzin.bhutia@tecnotree.com>
2025-10-03 19:22:14 +0530

    optimizing

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -4,48 +4,15 @@ import help_icon from "./help_icon.png";
 import logo_big from "./logo_big.svg";
 import logo_icon from "./logo_icon.svg";
 import logo from "./logo.png";
-import profile_richard from "./profile_richard.png";
-import profile_alison from "./profile_alison.png";
-import profile_enrique from "./profile_enrique.png";
-import profile_marco from "./profile_marco.png";
-import profile_martin from "./profile_martin.png";
-import search_icon from "./search_icon.png";
-import send_button from "./send_button.svg";
-import menu_icon from "./menu_icon.png";
-import arrow_icon from "./arrow_icon.png";
-import code from "./code.svg";
 import bgImage from "./bgImage.svg";
-import pic1 from "./pic1.png";
-import pic2 from "./pic2.png";
-import pic3 from "./pic3.png";
-import pic4 from "./pic4.png";
-import img1 from "./img1.jpg";
-import img2 from "./img2.jpg";
 
 const assets = {
   avatar_icon,
-  gallery_icon,
   help_icon,
   logo_big,
   logo_icon,
   logo,
-  profile_richard,
-  profile_alison,
-  profile_enrique,
-  profile_marco,
-  profile_martin,
-  search_icon,
-  send_button,
-  menu_icon,
-  arrow_icon,
-  code,
-  bgImage,
-  pic1,
-  pic2,
-  pic3,
-  pic4,
-  img1,
-  img2,
+  bgImage
 };
 
 export default assets;
